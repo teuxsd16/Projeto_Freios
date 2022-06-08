625c0577b3d8e504c1a6318b9d17c7076b56570e
thiago22203 <106891178+thiago22203@users.noreply.github.com>
2022-06-08 14:41:59 -0300

    Update and rename funcoes e adicoes.txt to funcoes e adicoes.ino

--- funcoes e adicoes.ino
+++ funcoes e adicoes.ino
@@ -3,25 +3,31 @@ void perigo() \\para quando o freio passar da temp. de perda de freio
 {
 digitalWrite(11, LOW);
 digitalWrite(12, LOW);
-digitalWrite(13, HIGH);
+digitalWrite(13, HIGH); // liga led vermelho
 lcd.setCursor(0,1);
 lcd.println("FREIO SUPERAQ."); \\Freio superaquecido
+tone(7,262,250);
+tone(7,294,250);
 delay(500);
 lcd.setCursor(0,1); \\ faz a tela ficar piscando
 lcd.println(" "); \\ apaga o que foi escrito anteriormente para ficar piscando
+tone(7,262,250);
+tone(7,294,250);
 delay(500);
 }
 /*\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\FUNCAO CUIDADO\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\*/
 void cuidado() \\ para quando o freio estiver quase no limite
 {
 digitalWrite(11, LOW);
-digitalWrite(12, HIGH);
+digitalWrite(12, HIGH); // liga led amarelo
 digitalWrite(13, LOW);
 lcd.setCursor(0,1);
 lcd.println("PRE LIMITE");
+tone(7,440,500); // ativa o buzzer numa frequencia 440Hz (Lá) por 500 ms. tone(pino,freq.,tempo em ms)
 delay(500);
 lcd.setCursor(0,1);
 lcd.println("CUIDADO"); \\ muda entre PRE LIMITE e CUIDADO
+tone(7,440,500);
 delay(500);
 }
 /*\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\FUNCAO TEMPERATURA MEDIANA\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\*/
@@ -29,7 +35,7 @@ void normal() \\para quando o feio estiver nem quente nem frio
 {
 lcd.setCursor(0,1);
 lcd.println("FREIO Cond. Nor"); \\ freio em condicao normal
-digitalWrite(11, HIGH);
+digitalWrite(11, HIGH); // liga led verde
 digitalWrite(12, LOW);
 digitalWrite(13, LOW);
 }
@@ -38,7 +44,7 @@ void suavedade() \\ para quando o freio nao estiver quente
 {
 lcd.setCursor(0,1);
 lcd.println("Freio Resfriado");
-digitalWrite(11, HIGH);
+digitalWrite(11, HIGH); // liga led verde
 digitalWrite(12, LOW);
 digitalWrite(13, LOW);
 }
